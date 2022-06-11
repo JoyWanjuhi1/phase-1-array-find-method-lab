dea675db1428034040fb41d85ea9645327a30269
Joy Wanjuhi <joywanjuhi@gmail.com>
2022-06-11 20:31:33 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+ function superBowlWin(item){
+   for (let name of item) {
+      if(name.result==="W"){
+      console.log(name.year)
+    } 
+     }
+  }
+function superbowlWin(record){
+    let result = record.find(record=>record.result==="W")
+    return result ? result.year: undefined;
+    
+}
